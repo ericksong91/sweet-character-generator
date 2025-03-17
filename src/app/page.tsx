09cae9a640c0312840e8a7e14fb23c2165838400
Eric Song <ericksong91@gmail.com>
2025-03-17 17:05:59 -0400

    Fixing typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Preview from "./_components/Preview";
 function App() {
   return (
     <div className="app min-h-screen p-4 bg-slate-500 ">
-      <main> className="main container flex flex-col flex-grow items-center gap-3 mx-auto p-5 bg-slate-200">
+      <main className="main container flex flex-col flex-grow items-center gap-3 mx-auto p-5 bg-slate-200">
         <h1 className="heading font-extrabold text-7xl tracking-widest">Sweet Character Creator</h1>
         <Preview />
       </main>
