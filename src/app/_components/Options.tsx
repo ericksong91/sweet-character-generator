32fd4066cc34881fcb9511213d099c22bddb9d44
Jared Patterson <jpwesley34@gmail.com>
2025-04-10 09:30:23 -0700

    Adjust selection tab coloration and add title to selection cards.

--- src/app/_components/Options.tsx
+++ src/app/_components/Options.tsx
@@ -35,7 +35,7 @@ function Options() {
             </h2>
             <div>
                 {selections.map((selection) => (
-                    <button className="bg-amber-600/75 hover:bg-amber-900/75 text-white px-4 py-2 m-1 rounded-b-3xl cursor-pointer" key={selection.id} onClick={() => handleSelectClick(selection)}>
+                    <button className="bg-amber-600/75 hover:bg-amber-900/75 focus:bg-amber-900/95 text-white px-4 py-2 m-1 rounded-b-3xl cursor-pointer" key={selection.id} onClick={() => handleSelectClick(selection)}>
                         {selection.name}
                     </button>
                 )
--- src/app/_components/SelectionCard.tsx
+++ src/app/_components/SelectionCard.tsx
@@ -14,25 +14,29 @@ function SelectionCard({name, imgsrc, id}: SelectionDisplayProps) {
             <img 
                 className=" object-contain h-32 w-32 rounded border border-amber-500 m-0.5 "
                 src={imgsrc}
-                alt={name}/>
+                alt={name}
+                title={name}/>
         </div>
         <div className="inline-grid  w-auto h-auto gap-1">
             <img 
                 className=" object-contain h-32 w-32 rounded border border-amber-500 m-0.5 "
                 src={imgsrc}
-                alt={name}/>
+                alt={name}
+                title={name}/>
         </div>
         <div className="inline-grid  w-auto h-auto gap-1">
             <img 
                 className=" object-contain h-32 w-32 rounded border border-amber-500 m-0.5 "
                 src={imgsrc}
-                alt={name}/>
+                alt={name}
+                title={name}/>
         </div>
-        <div className="inline-grid  w-auto h-auto gap-1 ">
+        <div className="inline-grid  w-auto h-auto gap-1">
             <img 
                 className=" object-contain h-32 w-32 rounded border border-amber-500 m-0.5 "
                 src={imgsrc}
-                alt={name}/>
+                alt={name}
+                title={name}/>
         </div>
         </>
     )
